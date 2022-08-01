252ebf3d01db0cab181e00bc85cb397291d7000e
Felix <104333119+F3LixMin@users.noreply.github.com>
2022-08-01 14:31:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21008740/democustomcontactlist/MainActivity.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.c346.id21008740.democustomcontactlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lvContact;
+    ArrayList<Contact> alContactList;
+    CustomAdapter caContact;
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        iv = findViewById(R.id.iv);
+
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(parent_context).load(imageUrl).into(iv);
+        setContentView(R.layout.activity_main);
+        lvContact = findViewById(R.id.listViewCOntacts);
+        alContactList = new ArrayList<>();
+        Contact item1 = new Contact("Mary", 65, 1234567, 'F');
+        Contact item2 = new Contact("Ken", 65, 7654321, 'M');
+        alContactList.add(item1);
+        alContactList.add(item2);
+        caContact = new CustomAdapter(this, R.layout.row, alContactList);
+        lvContact.setAdapter(caContact);
+    }
+
+}
